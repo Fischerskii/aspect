8f3dfe91ccb51add89eebee42656e7479fbeef4e
Pavel Trofimov <r-half@yandex.ru>
2021-12-01 23:40:57 +0300

    add annotation @Around

--- src/main/java/ru/iteco/aspecthomework/externalServiceApplication/aspect/CheckRequestAspect.java
+++ src/main/java/ru/iteco/aspecthomework/externalServiceApplication/aspect/CheckRequestAspect.java
@@ -2,6 +2,7 @@ package ru.iteco.aspecthomework.externalServiceApplication.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,6 +18,7 @@ public class CheckRequestAspect {
     @Value("${id-not-process}")
     private Integer id;
 
+    @Around("allMethodMarkCheckRequestAnnotation(externalInfo)")
     public void aroundAllMethodMarkCheckRequestAnnotationAndHaveExternalInfoInArg(ProceedingJoinPoint proceedingJoinPoint,
                                                                                   ExternalInfo externalInfo) throws Throwable {
         log.info("CHECK REQUEST: {} with {}", proceedingJoinPoint.getSignature().toShortString(), externalInfo);
